de2d69b73f352cbadaac4a302273b658bc69a4e8
Disruption Joe <joe@disruptionjoe.com>
2026-03-24 11:43:57 -0500

    rerouting  for booking path

---
+++ booking-links.js
@@ -0,0 +1,6 @@
+window.disruptionJoeBookingLinks = Object.freeze({
+  general: "https://calendar.app.google/dWVrARbatZMTanH2A",
+  discovery: "https://calendar.app.google/DWDdQNBmNGkYyAzs9",
+  aiSession60: "https://calendar.app.google/199L9BNcWZBW41nd6",
+  aiSession90: "https://calendar.app.google/6vjjE5VcCQYW2F7VA"
+});
