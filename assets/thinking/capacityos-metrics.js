a8a6aca2cccb888e8e1a941567b31978a809df89
Disruption Joe <35829692+disruptionjoe@users.noreply.github.com>
2026-07-30 01:02:03 -0500

    Update CapacityOS website activity metrics

--- assets/thinking/capacityos-metrics.js
+++ assets/thinking/capacityos-metrics.js
@@ -2,11 +2,11 @@
   "use strict";
 
   window.DJC_CAPACITYOS_METRICS = Object.freeze({
-  "asOf": "2026-07-29",
-  "synchronizedRepositories": 37,
-  "trackedFiles": 17118,
-  "commitsLastSevenDays": 1762,
-  "trackedAgentRuns": 3832,
-  "thinkingWikiGraphLinks": 935
+  "asOf": "2026-07-30",
+  "synchronizedRepositories": 38,
+  "trackedFiles": 17414,
+  "commitsLastSevenDays": 1401,
+  "trackedAgentRuns": 3859,
+  "thinkingWikiGraphLinks": 1015
 });
 })();
